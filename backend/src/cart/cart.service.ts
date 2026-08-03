6a796c763fa335185174b5c24b4c0a6c62e86d80
nbur1007 <nburhala@protonmail.com>
2026-08-03 15:45:02 +0300

    Add/Remove From Cart 3
    Working out the logic on adding and removing items from the cart.

--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -53,7 +53,87 @@ export class CartService {
     }
   }
 
-  async addToCart(userId: string, cartItem: CartItemDto) {}
+  async addToCart(userId: string, cartItem: CartItemDto) {
+    const cart = await this.prismaService.cart.findUnique({
+      where: { userId },
+      select: { id: true },
+    });
 
-  async removeFromCart(userId: string, cartItem: CartItemDto) {}
+    if (!cart) {
+      throw new HttpException('Cart not found.', HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      const updatedCart = await this.prismaService.cartItem.upsert({
+        where: {
+          cartId_productId: {
+            cartId: cart.id,
+            productId: cartItem.productId,
+          },
+        },
+        create: {
+          cartId: cart.id,
+          productId: cartItem.productId,
+          quantity: cartItem.quantity,
+        },
+        update: {
+          quantity: { increment: cartItem.quantity },
+        },
+        include: { product: true },
+      });
+      return updatedCart;
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2003') {
+          throw new HttpException('Product not found.', HttpStatus.NOT_FOUND);
+        }
+      }
+      throw err;
+    }
+  }
+
+  async removeFromCart(userId: string, decreaseRequest: CartItemDto) {
+    const cart = await this.prismaService.cart.findUnique({
+      where: { userId },
+      select: { id: true },
+    });
+
+    if (!cart) {
+      throw new HttpException('Cart not found.', HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      const item = await this.prismaService.cartItem.findFirst({
+        where: {
+          cartId: cart.id,
+          productId: decreaseRequest.productId,
+        },
+      });
+
+      if (!item) {
+        throw new HttpException('Item not in cart.', HttpStatus.NOT_FOUND);
+      }
+
+      const quantAfterDecrease = item.quantity - decreaseRequest.quantity;
+
+      if (quantAfterDecrease <= 0) {
+        return await this.prismaService.cartItem.delete({
+          where: { id: item.id },
+        });
+      } else {
+        return await this.prismaService.cartItem.update({
+          where: { id: item.id },
+          data: { quantity: quantAfterDecrease },
+        });
+      }
+
+      item;
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === '') {
+        }
+      }
+      throw err;
+    }
+  }
 }
