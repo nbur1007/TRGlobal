e1055cd8b64129149e9b51865d12c100efdd4edd
nbur1007 <nburhala@protonmail.com>
2026-08-04 11:45:07 +0300

    Order Basics
    Added some basic boilerplate declarations to the order controller and created DTO file.

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Patch, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { ModerateThrottle } from '../common/decorators/custom-throttler.decorator';
+import { Roles } from '../auth/guards/roles/roles.decorator';
+import { Role } from 'generated/prisma/enums';
 
 @Controller('order')
 export class OrderController {
   constructor(private orderService: OrderService) {}
+
+  @Post('create-order')
+  @ModerateThrottle()
+  @Roles(Role.CUSTOMER)
+  createOrder() {}
+
+  @Patch('update-order-status')
+  @Roles(Role.ADMIN)
+  updateOrderStatus() {}
 }
