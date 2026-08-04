c0a335f6cf6faf3eba31b251a6346701f149ec21
nbur1007 <nburhala@protonmail.com>
2026-08-04 16:58:23 +0300

    Order History Implementation
    Completed the implementation of user order history retrieval including pagination.

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Patch, Post, Req } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Query, Req } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { ModerateThrottle } from '../common/decorators/custom-throttler.decorator';
 import { Roles } from '../auth/guards/roles/roles.decorator';
 import { Role } from 'generated/prisma/enums';
 import type { Request } from 'express';
+import { PaginationDto } from '../catalogue/dto/product.dto';
 
 @Controller('order')
 export class OrderController {
@@ -12,9 +13,9 @@ export class OrderController {
   @Get('history')
   @ModerateThrottle()
   @Roles(Role.CUSTOMER)
-  getHistory(@Req() req: Request) {
+  getHistory(@Req() req: Request, @Query() paginationDto: PaginationDto) {
     const user = req.user as { id: string };
-    return this.orderService.getHistory(user.id);
+    return this.orderService.getHistory(user.id, paginationDto);
   }
 
   @Post('create-order')
--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { PaginationDto } from '../catalogue/dto/product.dto';
 
 @Injectable()
 export class OrderService {
+  constructor(private prismaService: PrismaService) {}
 
+  async getHistory(userId: string, paginationDto: PaginationDto) {
+    const { skip, take } = paginationDto;
 
-    getHistory(userId: string){}
+    const [orders, total] = await this.prismaService.$transaction([
+      this.prismaService.order.findMany({
+        where: { userId },
+        orderBy: { createdAt: 'desc' },
+        include: { items: true },
+        skip,
+        take,
+      }),
+      this.prismaService.order.count({
+        where: { userId },
+      }),
+    ]);
+
+    return {
+      orders,
+      total,
+      skip,
+      take,
+      hasMore: skip + orders.length < total,
+    };
+  }
 }
