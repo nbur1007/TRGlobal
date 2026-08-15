132e5420cfb9cfe79695ffb98a6e006a746cb95b
nbur1007 <nburhala@protonmail.com>
2026-08-15 13:42:31 +0300

    Cart Wrappers
    Finished implementing the wrappers for api interaction with cart module endpoints.

--- frontend/src/api/cart.ts
+++ frontend/src/api/cart.ts
@@ -1,5 +1,14 @@
-export function getCart() {}
+import { api } from "./client";
+import type { Cart, CartItem } from "./types";
 
-export function addToCart() {}
+export function getCart(userId: string) {
+  return api.get<Cart>("/cart", { userId });
+}
 
-export function removeFromCart() {}
+export function addToCart(data: CartItem) {
+  return api.patch<Cart>("/cart/add-to-cart", data);
+}
+
+export function removeFromCart(data: CartItem) {
+  return api.patch<Cart>("/cart/remove-from-cart", data);
+}
