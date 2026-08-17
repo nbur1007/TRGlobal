fb1c8b7be92370d86a574234a3f6fd574f3d8556
nbur1007 <nburhala@protonmail.com>
2026-08-17 05:25:43 +0300

    Consistant Naming

--- frontend/src/api/order.ts
+++ frontend/src/api/order.ts
@@ -29,6 +29,6 @@ export function updateOrderStatus(newStatus: UpdateOrderData) {
   return api.patch<Order>("/order/update-order-status", newStatus)
 }
 
-export function requestCancel(orderId: string) {
-  return api.patch<Order>("/order/cancel-order", orderId);
+export function requestCancel(id: string) {
+  return api.patch<Order>("/order/cancel-order", id);
 }
